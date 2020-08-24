0d7a3493b55aaf1186e545630d8568a57803beb7
Alma Flores <flores.almalee@gmail.com>
2020-08-24 20:54:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,15 @@
 class Dog 
   def initialize(name, breed)
     @name = name
-    @breed = breed
+    @breed = 'Mutt'
+  end
+  
+  def name=(name)
+    @name = name
+  end 
+  
+  def name
+    @name
   end
 end
 
